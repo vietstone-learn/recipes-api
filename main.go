deedc3581e7f79a9793fdc5c9dc56be0af578337
Viet Nguyen Tran <vietstone.ng@gmail.com>
2025-03-19 04:42:08 +0700

    recipe data model

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
 	router.Run(":8080")
+}
+
+type Recipe struct {
+	Name        string    `json:"name"`
+	Tags        []string  `json:"tag"`
+	Ingredients []string  `json:"ingredients"`
+	PublishedAt time.Time `json:"publishedAt"`
 }
